db742dc4be365243167e3001b2dc784287d9c661
김이화 <@DESKTOP-OAHFS92>
2023-07-06 17:30:06 +0900

    practice03

--- sql-practices/practice03.sql
+++ sql-practices/practice03.sql
@@ -42,19 +42,60 @@ order by a.first_name;
 -- ‘Technique Leader’의 직책으로 과거에 근무한 적이 있는 모든 사원의 사번과 이름을 출력하세요. 
 -- 현재 ‘Technique Leader’의 직책으로 근무하는 사원은 고려하지 않습니다.
 -- 이름은 first_name과 last_name을 합쳐 출력 합니다.
-select *
-	from 
+select a.emp_no, concat(a.first_name, ' ', a.last_name)
+	from employees a, titles b
+where a.emp_no = b.emp_no
+	and b.to_date != '9999-01-01'
+    and b.title != 'Technique Leader';
+    
 -- 문제6.
 -- 직원 이름(last_name) 중에서 S(대문자)로 시작하는 직원들의 이름, 부서명, 직책을 조회하세요.
+select a.last_name, c.dept_name, d.title
+	from employees a, dept_emp b, departments c, titles d
+where a.emp_no = b.emp_no
+	and b.dept_no = c.dept_no
+    and a.emp_no = d.emp_no
+    and a.last_name like 'S%';
 
 -- 문제7.
 -- 현재, 직책이 Engineer인 사원 중에서 현재 급여가 40000 이상인 사원을 급여가 큰 순서대로 출력하세요.
+select a.emp_no, c.first_name, b.salary
+	from titles a, salaries b, employees c
+where a.emp_no = b.emp_no
+	and a.emp_no = c.emp_no
+	and a.to_date = '9999-01-01'
+    and b.to_date = '9999-01-01'
+    and a.title = 'Engineer'
+    and b.salary >= 40000
+order by b.salary desc;
 
 -- 문제8.
 -- 현재 급여가 50000이 넘는 직책을 직책, 급여로 급여가 큰 순서대로 출력하시오
+select b.title, a.salary
+	from salaries a, titles b
+where a.emp_no = b.emp_no
+	and a.to_date = '9999-01-01'
+    and b.to_date = '9999-01-01'
+    and a.salary > 50000
+order by a.salary desc;
 
 -- 문제9.
 -- 현재, 부서별 평균 연봉을 연봉이 큰 부서 순서대로 출력하세요.
+select c.dept_name, avg(a.salary)
+	from salaries a, dept_emp b, departments c
+where a.emp_no = b.emp_no
+	and b.dept_no = c.dept_no
+    and a.to_date = '9999-01-01'
+    and b.to_date = '9999-01-01'
+group by c.dept_name
+order by avg(a.salary) desc;
 
 -- 문제10.
 -- 현재, 직책별 평균 연봉을 연봉이 큰 직책 순서대로 출력하세요.
+select a.title, avg(b.salary)
+	from titles a, salaries b
+where a.emp_no = b.emp_no
+	and a.to_date = '9999-01-01'
+    and b.to_date = '9999-01-01'
+group by a.title
+order by avg(b.salary) desc;
