c3fae5624fa33f0edbd71a9dc9ea9430bf6f60c3
김이화 <@DESKTOP-OAHFS92>
2023-07-06 16:57:45 +0900

    practice03

--- sql-practices/practice03.sql
+++ sql-practices/practice03.sql
@@ -2,21 +2,48 @@
 
 -- 문제 1. 
 -- 현재 급여가 많은 직원부터 직원의 사번, 이름, 그리고 연봉을 출력 하시오.
+select a.emp_no, b.first_name, a.salary
+	from salaries a, employees b
+where a.emp_no = b.emp_no
+	and a.to_date = '9999-01-01'
+order by a.salary desc;
 
 -- 문제2.
 -- 전체 사원의 사번, 이름, 현재 직책을 직원의 이름 순서로 출력하세요.
+select a.emp_no, a.first_name, b.title
+	from employees a, titles b
+where a.emp_no = b.emp_no
+	and b.to_date = '9999-01-01'
+order by a.first_name;
 
 -- 문제3.
 -- 전체 사원의 사번, 이름, 현재 부서를 이름 순서로 출력하세요.
+select a.emp_no, a.first_name, c.dept_name
+	from employees a, dept_emp b, departments c 
+where a.emp_no = b.emp_no
+	and b.dept_no = c.dept_no
+    and b.to_date = '9999-01-01'
+order by a.first_name;
 
 -- 문제4.
 -- 현재, 전체 사원의 사번, 이름, 연봉, 직책, 부서를 모두 이름 순서로 출력합니다.
+select a.emp_no, a.first_name, b.salary, c.title, e.dept_name
+	from employees a, salaries b, titles c, dept_emp d, departments e 
+where a.emp_no = b.emp_no
+	and a.emp_no = c.emp_no
+    and a.emp_no = d.emp_no
+    and d.dept_no = e.dept_no
+    and b.to_date = '9999-01-01'
+    and c.to_date = '9999-01-01' 
+    and d.to_date = '9999-01-01'
+order by a.first_name;
 
 -- 문제5.
 -- ‘Technique Leader’의 직책으로 과거에 근무한 적이 있는 모든 사원의 사번과 이름을 출력하세요. 
 -- 현재 ‘Technique Leader’의 직책으로 근무하는 사원은 고려하지 않습니다.
 -- 이름은 first_name과 last_name을 합쳐 출력 합니다.
-
+select *
+	from 
 -- 문제6.
 -- 직원 이름(last_name) 중에서 S(대문자)로 시작하는 직원들의 이름, 부서명, 직책을 조회하세요.
 
