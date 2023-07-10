721d036eecdb3c254f9fe14b54843b3779c64969
김이화 <@DESKTOP-OAHFS92>
2023-07-10 10:58:36 +0900

    DAO Pattern I

---
+++ jdbc-practice/src/main/java/hr/dao/EmployeeDao.java
@@ -0,0 +1,15 @@
+package hr.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import hr.vo.EmployeeVo;
+
+public class EmployeeDao {
+	public List<EmployeeVo> findByName(String keyword){
+		List<EmployeeVo> result = new ArrayList<>();
+		
+		
+		return result;
+	}
+}
